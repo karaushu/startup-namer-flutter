e1b0ea4ab03edf444cf19f024209f1bd6c187ca6
Andy <karandriy@gmail.com>
2021-07-01 13:07:12 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,13 +11,138 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
+      theme: ThemeData(
+        primaryColor: Colors.grey.shade300,
+      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute<void>(builder: (BuildContext context) {
+      final tiles = _saved.map(
+        (WordPair pair) {
+          return ListTile(
+            title: Text(
+              pair.asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              Icons.delete_forever,
+              color: Colors.red,
+            ),
+            onTap: () {
+              setState(() {
+                _saved.remove(pair);
+              });
+            },
+          );
+        },
+      );
+      final divided = tiles.isNotEmpty
+          ? ListTile.divideTiles(tiles: tiles, context: context).toList()
+          : <Widget>[
+              Container(
+                child: Text(
+                  'Empty List',
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+                padding: EdgeInsets.all(24),
+                alignment: Alignment.center,
+              )
+            ];
+
+      return Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: Text(
+            'Saved Suggestions',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: ListView(
+          children: divided,
+        ),
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Startup Name Generator',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          actions: [
+            IconButton(
+              onPressed: _pushSaved,
+              icon: Icon(Icons.list),
+            )
+          ],
+        ),
+        body: Center(
+          child: _buildSuggestions(),
         ),
       ),
     );
